2a8a8e7c4baf95cef25e243d4708915a9a0200e7
Clifford Wulfman <cwulfman@princeton.edu>
2014-06-17 07:17:40 -0400

    Adds script to walk a directory and transform MJP METS files into Collex RDF.

---
+++ transform2rdf.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+import os
+from string import Template
+import argparse
+
+
+xslfile = '/Users/cwulfman/work/Modnets/mjp2rdf.xsl'
+cmdTemplate = Template('saxon -s:$src -xsl:$xsl -o:$out')
+
+def transform_files(sourcedir, targetdir):
+    for root, dirs, files in os.walk(sourcedir):
+        target_subdir = '/'.join((targetdir.rstrip('/'), root.lstrip('/')))
+        for fname in files:
+            if fname.endswith('.xml'):
+                source = '/'.join((root, fname))
+                target = '/'.join((target_subdir, fname))
+                cmd = cmdTemplate.substitute(src=source, xsl=xslfile, out=target)
+                print "echo " + cmd
+                print cmd
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_dir", help="top-level directory of source xml files.")
+    parser.add_argument("-o", "--output_dir", help="target directory.")
+    args = parser.parse_args()
+
+    if args.input_dir and args.output_dir:
+        transform_files(args.input_dir, args.output_dir)
+
+
+
+
